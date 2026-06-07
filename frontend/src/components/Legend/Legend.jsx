2e144ae254a01ffbc6bcd039af2de95b148018cc
PrinceKay28748 <pkay28748@gmail.com>
2026-06-07 12:58:35 +0000

    mmmmm

--- frontend/src/components/Legend/Legend.jsx
+++ frontend/src/components/Legend/Legend.jsx
@@ -380,7 +380,7 @@ const Legend = forwardRef(function Legend(
     if (isStandalone && isIOS) {
       extraOffset = -34;
     } else if (isIOS) {
-      extraOffset = -41;
+      extraOffset = -47;
     } else {
       extraOffset = 0;
     }
